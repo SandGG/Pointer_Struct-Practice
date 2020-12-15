ffb8a5f889985be99271be18d6673b90664c0f73
Sandra <lissetdovah@gmail.com>
2020-12-14 21:46:54 -0600

    Pointer_Struct-Practice 1.1

--- main.go
+++ main.go
@@ -3,20 +3,21 @@ package main
 import "fmt"
 
 type figure struct {
-	height, width int
+	height, width *int
 	name          string
 }
 
 func main() {
-	rectangle := figure{5, 10, "Rectangle"}
+	var height, width int = 20, 10
+	rectangle := figure{&height, &width, "Rectangle"}
 	fmt.Println(rectangle)
 	area(&rectangle)
 	fmt.Println(rectangle)
 }
 
 func area(shape *figure) {
-	shape.height = 30
-	area := shape.height * shape.width
+	*shape.height = 30
+	area := *shape.height * *shape.width
 
 	fmt.Println(shape.name, area, "area")
 }
